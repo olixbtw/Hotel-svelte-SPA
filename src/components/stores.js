1eb9c7b621d9075db8b0c0a0e9aca2eb4b90569d
Alex Revchuk <sasrev@yahoo.com>
2019-08-02 02:09:38 +0300

    wrong data type for reservation

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store'
 
-export const reservedRoom = writable({})
+export const reservedRoom = writable([])
 export const roomsFilter = writable({})
 export const navHeight = writable()
