130baff9b6b5a00ebd933dac7201f006e3a3935d
Alex Revchuk <sasrev@yahoo.com>
2019-08-04 14:48:17 +0300

    filter ported

--- src/routes/rooms/_filter/filter.svelte
+++ src/routes/rooms/_filter/filter.svelte
@@ -17,33 +17,125 @@
   }
 </script>
 
-<style>
+<style lang="scss">
   .filter-button {
-    display: none;
+    width: 3rem;
+    height: 3rem;
+    font-size: 1.2rem;
+    line-height: 3rem;
+    text-align: center;
+    color: #dfdbdb;
+    background: #443941;
+    cursor: pointer;
+    position: absolute;
+    bottom: 100%;
+    right: 0;
   }
-  nav {
-    font-size: 0.7rem;
-    display: flex;
-    margin-bottom: 2rem;
-  }
-  nav > * {
-    border: 1px solid #000;
-    padding: 0.5rem;
-    line-height: 1.2;
-  }
-  section {
-    flex-grow: 1;
+  .filter-body {
+    position: absolute;
+    top: 5rem;
+    z-index: 500;
+    white-space: nowrap;
+    line-height: 2.2;
+
+    right: 0.5rem;
+    // @media (min-width: 1000px) {
+    // }
+    @media (min-width: 1680px) {
+      left: -3rem;
+    }
+
+    &.active {
+      nav {
+        max-height: 90vh;
+        max-width: 50vw;
+        opacity: 1;
+      }
+    }
+    nav {
+      margin: 0 0 0 auto;
+      transition: all 0.4s;
+      padding: 1rem 2.5rem;
+      color: #a19d9d;
+      background: rgba(68, 57, 65, 0.95);
+      opacity: 0;
+      max-height: 0;
+      max-width: 0;
+      overflow: hidden;
+      display: block;
+      float: right;
+    }
   }
   input,
   label {
+    cursor: pointer;
     display: inline-block;
   }
-  ul,
-  li {
-    list-style: circle;
+  input {
+    &[type="number"] {
+      width: 4rem;
+    }
+    &[name="filter_view"] {
+      display: none;
+
+      & + label {
+        color: #a19d9d;
+      }
+    }
+    &:checked + label {
+      color: #dfdbdb;
+    }
   }
-  ul {
-    padding-left: 20px;
+  label {
+    i {
+      margin-right: 0.5em;
+    }
+  }
+  h4 {
+    margin: 0.15em 0 0.25em -0.5rem;
+    color: #dfdbdb;
+  }
+  // .list-STYLED-FILTER {
+  //   .filter-button {
+  //     display: none;
+  //   }
+  //   nav {
+  //     font-size: 0.7rem;
+  //     display: flex;
+  //     margin-bottom: 2rem;
+  //   }
+  //   nav > * {
+  //     border: 1px solid #000;
+  //     padding: 0.5rem;
+  //     line-height: 1.2;
+  //   }
+  //   section {
+  //     flex-grow: 1;
+  //   }
+  //   input,
+  //   label {
+  //     display: inline-block;
+  //   }
+  //   ul,
+  //   li {
+  //     list-style: circle;
+  //   }
+  //   ul {
+  //     padding-left: 20px;
+  //   }
+  // }
+  input[name="filter_sort_order"] {
+    display: none;
+  }
+  label[for=filter_sort_order] {
+    color: red;
+    font-size: 1.5rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  section {
+    position: relative;
   }
 </style>
 
@@ -52,14 +144,13 @@
     <i class="fas fa-tasks" />
   </div>
   <nav>
-    <div class="filter">
+    <section class="filter">
       <div class="guests">
         <h4>Guests</h4>
         <Button on:click={red_guest}>-</Button>
         <input type="number" bind:value={$roomsFilter.guests.n} />
         <Button on:click={inc_guest}>+</Button>
       </div>
-      <br />
       <input
         id="filter_n_guests"
         type="checkbox"
@@ -81,19 +172,31 @@
         type="checkbox"
         bind:checked={$roomsFilter.amenities[2]} />
       <label for="filter_amenities">Завтрак в постель</label>
-    </div>
-    <div class="sort">
+    </section>
+    <section class="sort">
       <h4>Sort by</h4>
-      <Button on:click={() => ($roomsFilter.sort = '')}>Reset</Button>
+      <Button on:click={() => ($roomsFilter.sort = '')}>
+        <i class="fas fa-sync-alt" />
+        Reset
+      </Button>
       <br />
-      <input
-        id="filter_sort_order"
-        type="checkbox"
-        name="filter_sort_order"
-        bind:checked={$roomsFilter.reverse} />
-      <label for="filter_sort_order">{$roomsFilter.reverse}</label>
+      {#if $roomsFilter.sort}
+        <input
+          id="filter_sort_order"
+          type="checkbox"
+          name="filter_sort_order"
+          bind:checked={$roomsFilter.reverse} />
+        {#if $roomsFilter.reverse}
+          <label for="filter_sort_order" transition:fade>
+            <i class="fas fa-sort-amount-up-alt" />
+          </label>
+        {:else}
+          <label for="filter_sort_order" transition:fade>
+            <i class="fas fa-sort-amount-down-alt" />
+          </label>
+        {/if}
+      {/if}
 
-      <br />
       <input
         id="filter_sort_price"
         type="radio"
@@ -117,8 +220,8 @@
         name="filter_sort"
         bind:group={$roomsFilter.sort} />
       <label for="filter_sort_people">number of guests</label>
-    </div>
-    <div class="view">
+    </section>
+    <section class="view">
       <h4>View</h4>
       <input
         id="filter_view_list"
@@ -141,8 +244,8 @@
         <i class="fas fa-grip-horizontal" />
         cards
       </label>
-    </div>
-    <section>
+    </section>
+    <!-- <section>
       <h4>Data</h4>
       <ul>
         <li>$roomsFilter.guests.n {$roomsFilter.guests.n}</li>
@@ -151,6 +254,6 @@
         <li>$roomsFilter.sort {$roomsFilter.sort}</li>
         <li>$roomsFilter.view {$roomsFilter.view}</li>
       </ul>
-    </section>
+    </section> -->
   </nav>
 </div>
